28eeb3c671d57477865f9748908cf8c4ac5849b6
noppanut <noppanut.t@ku.th>
2020-05-22 15:33:18 +0700

    Add Fisheye caribration => lib_cam_caribration.py , Edit stitch.py , Add image after stitch

---
+++ MobileRobot/Image/lib_cam_caribrate.py
@@ -0,0 +1,137 @@
+import cv2
+assert cv2.__version__[0] == '3', 'The fisheye module requires opencv version >= 3.0.0'
+import numpy as np
+import os
+import glob
+import sys
+
+def undistort(img):
+    DIM , K , D = load_coefficients("/home/pat/HG_internship/MobileRobot/camera.yml")
+    DIM = tuple(DIM)
+    K = np.array(K)
+    D = np.array(D)
+    # img = cv2.imread(img_path)
+    h,w = img.shape[:2]
+    print("H : ",h," , W : ",w)
+    map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
+    undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+    # cv2.imshow("undistorted", undistorted_img)
+    # cv2.waitKey(0)
+    return undistorted_img
+
+def trim(frame):
+    #crop top
+    if not np.sum(frame[0]):
+        return trim(frame[1:])
+    #crop bottom
+    elif not np.sum(frame[-1]):
+        return trim(frame[:-2])
+    #crop left
+    elif not np.sum(frame[:,0]):
+        return trim(frame[:,1:]) 
+    #crop right
+    elif not np.sum(frame[:,-1]):
+        return trim(frame[:,:-2])    
+    return frame
+
+def save_coefficients(DIM, K, D , path):
+    """ Save the camera matrix and the distortion coefficients to given path/file. """
+    cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_WRITE)
+    cv_file.write("DIM" , DIM)
+    cv_file.write("K", K)
+    cv_file.write("D", D)
+    # note you *release* you don't close() a FileStorage object
+    cv_file.release()
+
+def load_coefficients(path):
+    """ Loads camera matrix and distortion coefficients. """
+    # FILE_STORAGE_READ
+    cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_READ)
+
+    # note we also have to specify the type to retrieve other wise we only get a
+    # FileNode object back instead of a matrix
+    DIM = cv_file.getNode("DIM").mat()
+    K = cv_file.getNode("K").mat()
+    D = cv_file.getNode("D").mat()
+
+    cv_file.release()
+    return [DIM, K , D]
+
+def caribrate(path,save=False):
+
+    CHECKERBOARD = (6,9)
+    subpix_criteria = (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1)
+    calibration_flags = cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC+cv2.fisheye.CALIB_CHECK_COND+cv2.fisheye.CALIB_FIX_SKEW
+    objp = np.zeros((1, CHECKERBOARD[0]*CHECKERBOARD[1], 3), np.float32)
+    objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
+    _img_shape = None
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+    images = glob.glob(path)
+    for fname in images:
+        img = cv2.imread(fname)
+        if _img_shape == None:
+            _img_shape = img.shape[:2]
+        else:
+            assert _img_shape == img.shape[:2], "All images must share the same size."
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
+            cv2.cornerSubPix(gray,corners,(3,3),(-1,-1),subpix_criteria)
+            imgpoints.append(corners)
+
+    N_OK = len(objpoints)
+    K = np.zeros((3, 3))
+    D = np.zeros((4, 1))
+    rvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_OK)]
+    tvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_OK)]
+    rms, _, _, _, _ = \
+        cv2.fisheye.calibrate(
+            objpoints,
+            imgpoints,
+            gray.shape[::-1],
+            K,
+            D,
+            rvecs,
+            tvecs,
+            calibration_flags,
+            (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 1e-6)
+        )
+    print("Found " + str(N_OK) + " valid images for calibration")
+    print("DIM=" + str(_img_shape[::-1]))
+    print("K=np.array(" + str(K.tolist()) + ")")
+    print("D=np.array(" + str(D.tolist()) + ")")
+
+    DIM = _img_shape[::-1]
+    K = np.array(K.tolist())
+    D = np.array(D.tolist())
+
+    if save:
+        # files = glob.glob("/home/pat/HG_internship/MobileRobot/camera.yml")
+        save_coefficients(DIM,K,D,"/home/pat/HG_internship/MobileRobot/camera.yml")
+
+    return [DIM , K ,D]
+
+
+def trim(frame):
+    #crop top
+    if not np.sum(frame[0]):
+        return trim(frame[1:])
+    #crop bottom
+    elif not np.sum(frame[-1]):
+        return trim(frame[:-2])
+    #crop left
+    elif not np.sum(frame[:,0]):
+        return trim(frame[:,1:]) 
+    #crop right
+    elif not np.sum(frame[:,-1]):
+        return trim(frame[:,:-2])    
+    return frame
+
+
+if __name__ == "__main__":
+
+    caribrate("/home/pat/HG_internship/MobileRobot/video/006/*.png",save=True)
--- MobileRobot/Image/stitch.py
+++ MobileRobot/Image/stitch.py
@@ -4,37 +4,30 @@ import imutils
 import cv2
 import time
 import numpy as np
-import cam_caribrate
+import lib_cam_caribrate
 import parser
 
-def trim(frame):
-    #crop top
-    if not np.sum(frame[0]):
-        return trim(frame[1:])
-    #crop bottom
-    elif not np.sum(frame[-1]):
-        return trim(frame[:-2])
-    #crop left
-    elif not np.sum(frame[:,0]):
-        return trim(frame[:,1:]) 
-    #crop right
-    elif not np.sum(frame[:,-1]):
-        return trim(frame[:,:-2])    
-    return frame
+name_counter = 0
 
-parser = argparse.ArgumentParser(description='Stitch')
 
-parser.add_argument('--file', type=str, required=False, help='YML file to load calibration matrices')
+def save_image(frame):
 
-args = parser.parse_args()
+    global name_counter
 
-mtx , dist = cam_caribrate.load_coefficients(args.file)
+    name_counter += 1
+
+    path = "/home/pat/HG_internship/MobileRobot/video/image_group/"+str(name_counter)+".png"
+
+    cv2.imwrite(path,frame)
+
+    return True
+
 
 cap = cv2.VideoCapture("/home/pat/HG_internship/MobileRobot/video/002_forward.mp4")
 
 success , image = cap.read()
 
-result = "as"
+result = None
 
 count = 0
 
@@ -56,155 +49,72 @@ while success:
         count += 1
         continue
 
-    success , image = cap.read()
-    image = cv2.imread("/home/pat/HG_internship/MobileRobot/video/006/batch_001.png")
     print(count)
 
-    # print(result)
-
-    if type(result) == type(None):
-        break
-
-    if count in [117]:
+    if count in []:
         count+=1
         continue
 
-    if result[0] == "a":
-        imageA = image
-        h,  w = imageA.shape[:2]
-        # imageA = imutils.resize(imageA, width=400)
-        # imageA = imageA[140:170 , 60:340]
-        # imageA = imutils.rotate_bound(imageA, 270)
-
-    elif add == 0:
-        print("Trigger image")
-        success , image = cap.read()
-        image = cv2.imread("/home/pat/HG_internship/MobileRobot/video/006/batch_001.png")
-        imageA = image
-        # imageA = imutils.resize(imageA, width=400)
-        # imageA = imageA[140:170 , 60:340]
-        # imageA = imutils.rotate_bound(imageA, 270)
+    if type(result) == type(None):
+        success , imageA = cap.read()
+        imageA = lib_cam_caribrate.undistort(imageA)
+        imageA = imutils.resize(imageA, width=400)
+        imageA = imageA[140:190 , 50:350]
+        imageA = imutils.rotate_bound(imageA, 270)
 
     else:
         imageA = result
 
-    # h,  w = imageA.shape[:2]
-    # newcameramtxA, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
-    # mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtxA,(w,h),5)
-    # dst = cv2.remap(imageA,mapx,mapy,cv2.INTER_LINEAR)
-
-    # # x,y,w,h = roi
-    # # dst = dst[y:y+h, x:x+w]
-    # imageA = dst
-
-    success , image = cap.read()
-    image = cv2.imread("/home/pat/HG_internship/MobileRobot/video/006/batch_001.png")
+    success , imageB = cap.read()
 
     while not success:
 
-        success , image = cap.read()
-        image = cv2.imread("/home/pat/HG_internship/MobileRobot/video/006/batch_001.png")
+        success , imageB = cap.read()
+
         print("waiting for success")
         break
 
-    imageB = image
     if type(imageB) == type(None):
         continue
 
-    h,  w = imageB.shape[:2]
-    newcameramtxB, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
-    mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtxB,(w,h),5)
-    dst = cv2.remap(imageB,mapx,mapy,cv2.INTER_LINEAR)
-    # x,y,w,h = roi
-    # dst = dst[y:y+h, x:x+w]
-    imageB = dst
-    # imageB = imutils.resize(imageB, width=400)
+    imageB = lib_cam_caribrate.undistort(imageB)
 
-    # imageB = imageB[140:180 , 50:350]
+    imageB = imutils.resize(imageB, width=400)
 
-    # imageB = imutils.rotate_bound(imageB, 270)
+    imageB = imageB[140:190 , 50:350]
+
+    imageB = imutils.rotate_bound(imageB, 270)
 
     # stitch the images together to create a panorama
-    # stitcher = Stitcher()
-    # try:
-    #     re = stitcher.stitch([imageA, imageB], showMatches=True)
+    stitcher = Stitcher()
+    re = stitcher.stitch([imageA, imageB], showMatches=True)
 
-    # except Exception:
-    #     continue
-    # if type(re) == type(None):
-    #     print("Can't stitch the picture")
-    #     continue
-    # (result, vis) = re
-    # result = trim(result)
+    if type(re) == type(None):
+        save_image(result)
+        result = None
+        continue
 
-    # show the images
+    (result , vis) = re
+
+    result = lib_cam_caribrate.trim(result)
 
     cv2.imshow("Image A", imageA)
     cv2.imshow("Image B", imageB)
-    # cv2.imshow("Result", result)
 
-    if cv2.waitKey(0) & 0xFF == ord('q'):
-        break
+    result = result[:,:len(result[0])-1]
+
+    cv2.imshow("Result", result)
 
     count += 1
-    add += 1
-    if add == 1:
-        # result = imutils.resize(result, width=400)
-        list_result.append(result)
-        add = 0
 
-    # except Exception as e:
+    if cv2.waitKey(0) != ord("q"):
+        
+        continue
 
-    #     print(e)
-    #     break
+    else:
 
+        break
 
-
-# list_result_2 = []
-
-# for i in range(len(list_result)-1):
-
-#     stitcher = Stitcher()
-#     try:
-#         (result, vis) = stitcher.stitch([list_result[i+1], list_result[i]], showMatches=True)
-#     except ValueError:
-#         (result, vis) = stitcher.stitch([list_result[i], list_result[i+1]], showMatches=True)
-#     except TypeError:
-#         continue
-#     result = trim(result)
-#     list_result_2.append(result)
-
-# # cv2.imshow("Keypoint Matches", vis)
-# num = 0
-# for i in list_result_2:
-#     # cv2.imshow("Result form list "+str(num), i)
-#     # image_namee = "/home/pat/HG_internship/MobileRobot/video/image_group/image"+str(num)+".png"
-#     # cv2.imwrite(image_namee,i)
-#     # num += 1
+    
 
 
-
-# list_result_3 = []
-
-# for i in range(len(list_result)-1):
-
-#     stitcher = Stitcher()
-#     try:
-#         (result, vis) = stitcher.stitch([list_result[i+1], list_result[i]], showMatches=True)
-#     except ValueError:
-#         (result, vis) = stitcher.stitch([list_result[i], list_result[i+1]], showMatches=True)
-#     except TypeError:
-#         continue
-#     result = trim(result)
-#     list_result_2.append(result)
-
-# # cv2.imshow("Keypoint Matches", vis)
-# num = 0
-# for i in list_result_2:
-#     cv2.imshow("Result form list "+str(num), i)
-#     image_namee = "/home/pat/HG_internship/MobileRobot/video/image_group/image"+str(num)+".png"
-#     cv2.imwrite(image_namee,i)
-#     num += 1
-
-# cv2.imshow("Result", result)
-# cv2.waitKey(0)
